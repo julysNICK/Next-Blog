7cad97ea46629c39be7643a65b5e442ad8094c51
Julys Martins <julystube@gmail.com>
2021-05-17 20:06:49 -0300

    modifier stories

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -10,18 +10,16 @@ export type CommentsProps = {
 
 export const Comments = ({ id, slug, title, allowComments }: CommentsProps) => {
   if (!allowComments) return null;
+  const config = {
+    url: `https://next-blog-lac-eight.vercel.app/post/${slug}`,
+    identifier: id,
+    title: title,
+    language: 'pt_BR',
+  };
 
   return (
     <Styled.Wrapper>
-      <DiscussionEmbed
-        shortname="blog-do-julys-martins"
-        config={{
-          url: `/post/${slug}`,
-          identifier: id,
-          title: title,
-          language: 'pt_BR',
-        }}
-      />
+      <DiscussionEmbed shortname="blog-do-julys-martins" config={config} />
     </Styled.Wrapper>
   );
 };
